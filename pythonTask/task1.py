0fe1ba183de4c2b6549745197a0df400b13ce82d
muhammadhamza974 <muhammadhamza974@gmail.com>
2021-11-18 12:04:33 +0500

    first commit

---
+++ pythonTask/task1.py
@@ -0,0 +1,65 @@
+#Task 01
+
+# list1 = ['m','na','i','ke']
+# list2 = ['y','me','s','lly']
+# cont = []
+# count=-1
+# for i in list1:
+#     count = count+1
+#     if count == len(list1):
+#         count = 0
+#         break
+#     res = list1[count]+list2[count]
+#     cont.append(res)
+# print(str(cont))
+
+# list3 = [i + j for i, j in zip(list1, list2)]
+# print(list3)
+
+#Task 02
+#Print list with another list like pointer
+list1 = ['Hello', 'Take']
+list2 = ['Dear', 'Sir']
+cont = []
+cont1 = []
+count=-1
+for i in list1:
+    count = count+1
+    if count == len(list1):
+        count = 0
+        break
+    res = list1[0]+list2[count]
+    cont.append(res)
+    res1 = list1[1]+list2[count]
+    cont1.append(res1)
+print(str(cont+cont1))
+
+# Task 03
+# print vertically and reverse list
+
+# list1 = [10,20,30,40]
+# list2 = [100,200,300,400]
+# list2.reverse()
+#
+# for l1,l2 in zip(list1,list2):
+#     print(l1,l2)
+
+# Task 04
+#Squre of list item
+
+# number = [1,2,3,4,5,6,7]
+# cont = []
+# for i in number:
+#     sqr = i**2
+#     cont.append(sqr)
+# print(cont)
+
+#Task 05
+# Empty string remove fro list
+
+# list1 = ["Mike","","Emma","Kelly","","Brad"]
+# cont = []
+# for i in list1:
+#     if i != "":
+#         cont.append(i)
+# print(cont)
